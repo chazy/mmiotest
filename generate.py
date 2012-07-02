d459b4ae8a1741d117ebceda0c59604af6be08c5
Christoffer Dall <cdall@cs.columbia.edu>
2012-07-02 01:52:40 -0400

    Improve on still incomplete generate.py
    Signed-off-by: Christoffer Dall <cdall@cs.columbia.edu>

--- generate.py
+++ generate.py
@@ -46,22 +46,22 @@ LS_core_list = [
     LS_core("str",     True,     32,      Extra.NONE,      False ),
     LS_core("ldrt",    False,    32,      Extra.UNPRIV,    False ),
     LS_core("strt",    True,     32,      Extra.UNPRIV,    False ),
-    LS_core("ldrex"    False,    32,      Extra.EX,        False ),
-    LS_core("strex",   True,     32,      Extra.EX,        False ),
+    LS_core("ldrex",   False,    32,      Extra.EXCL,      False ),
+    LS_core("strex",   True,     32,      Extra.EXCL,      False ),
     LS_core("strh",    True,     16,      Extra.NONE,      False ),
     LS_core("strht",   True,     16,      Extra.UNPRIV,    False ),
-    LS_core("strexh",  True,     16,      Extra.EX,        False ),
+    LS_core("strexh",  True,     16,      Extra.EXCL,      False ),
     LS_core("ldrh",    False,    16,      Extra.NONE,      False ),
     LS_core("ldrht",   False,    16,      Extra.UNPRIV,    False ),
-    LS_core("ldrexh",  False,    16,      Extra.EX,        False ),
+    LS_core("ldrexh",  False,    16,      Extra.EXCL,      False ),
     LS_core("ldrsh",   False,    16,      Extra.NONE,      True  ),
     LS_core("ldrsht",  False,    16,      Extra.UNPRIV,    True  ),
     LS_core("strb",    True,      8,      Extra.NONE,      False ),
     LS_core("strbt",   True,      8,      Extra.UNPRIV,    False ),
-    LS_core("strexb",  True,      8,      Extra.EX,        False ),
+    LS_core("strexb",  True,      8,      Extra.EXCL,      False ),
     LS_core("ldrb",    False,     8,      Extra.NONE,      False ),
     LS_core("ldrbt",   False,     8,      Extra.UNPRIV,    False ),
-    LS_core("ldrexb",  False,     8,      Extra.EX,        False ),
+    LS_core("ldrexb",  False,     8,      Extra.EXCL,      False ),
     LS_core("ldrsb",   False,     8,      Extra.NONE,      True  ),
     LS_core("ldrsbt",  False,     8,      Extra.UNPRIV,    True  ),
     LS_core("ldrd",    False,    64,      Extra.NONE,      False ),
@@ -70,7 +70,43 @@ LS_core_list = [
     LS_core("strexd",  True,     64,      Extra.NONE,      False ),
     ]
 
+class LDM:
+    def __init__(self, instr, store, arm_only, stack):
+        self.instr = instr
+        self.store = store
+        self.arm_only = arm_only
+        self.stack = stack
+        pass
+
+#
+# List of load/store multiple instructions
+#
+#        Instr    | Store   |  ARM-only | Stack
+#        --------------------------------------
+LDM_list = [
+    LDM("ldmia",    False,     False,     False ),
+    LDM("ldmda",    False,     True,      False ),
+    LDM("ldmdb",    False,     False,     False ),
+    LDM("ldmib",    False,     True,      False ),
+    LDM("pop",      False,     False,     True  ),
+    LDM("push",     True,      False,     True  ),
+    LDM("stmia",    True,      False,     False ),
+    LDM("stmda",    True,      True,      False ),
+    LDM("stmdb",    True,      False,     False ),
+    LDM("stmib",    True,      True,      False ),
+    ]
+
+LDM_reglist = []
+for first_reg in range(0, 12):
+    for n in range(first_reg, 12):
+        LDM_reglist.append(", ".join("r%d" % (i) for i in range(first_reg, n)))
+
 
 if __name__ == "__main__":
     for inst in LS_core_list:
         print inst.instr
+    for inst in LDM_list:
+        print inst.instr
+    for reglist in LDM_reglist:
+        print reglist
+
